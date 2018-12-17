16e2449f2803f9984f9915d3782480e04eb94f2f
Kn <kn@ecample.com>
2018-12-18 00:14:58 +0200

    REACT

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class Portfolio extends Component{
   render() {
+    return(
+    <p>Hello</p>
+    );
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      portfolioCount:0
+    }
+  }
+
+  addPortfolio(){
+    if(this.state.portfolioCount<10){
+      this.setState({portfolioCount:this.state.portfolioCount+1});
+    }
+  }
+
+  render() {
+    const portfolios = [];
+    portfolios.push(<Portfolio />);
+    for(let i = 0; i < this.state.portfolioCount; i++) {
+      portfolios.push(<Portfolio />);
+    }
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button onClick={()=>this.addPortfolio()}>Add Portfolio {this.state.portfolioCount}</button>
+          SPMS
+          <p>Lol</p>
+          {portfolios}
         </header>
       </div>
     );
