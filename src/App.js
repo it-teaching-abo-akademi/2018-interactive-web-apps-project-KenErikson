8933c871d7c3f7ed5aae174de3da0d8dabd28f6e
Ken Erikson <ken@hibox.fi>
2018-12-22 15:21:29 +0200

    Component split

--- src/App.js
+++ src/App.js
@@ -1,48 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import Portfolio from './components/Portfolio'
 
-class TextInput extends Component{
-  handleInput() {
-    var input = React.findDOMNode(this.refs.userInput)
-    this.props.saveInput(input.value)
-    input.value = ''
-  }
-  render() {
-    var label = this.props.label
-    return (
-      <div class="form-group">
-        <input 
-          type="text" 
-          class="form-control" 
-          id="input-{ label }" 
-          ref="userInput"
-         />
-        <button onClick={ this.handleInput }>Save</button>
-      </div>
-    )
-  }
-}
-
-
-class Portfolio extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userIsEditing:false,
-      title:"Portfolio "+props.id
-    }
-  }
-  render() {
-    return (
-      <div className="App-Portfolio">
-        <TextInput className="Portfolio-Title">Potrfolio {this.state.title}</TextInput>
-        <button className="Portfolio-Button">Show in €</button>
-        <button className="Portfolio-Button">Show in $</button>
-        <button className="Portfolio-Button Exit" onClick={() => this.props.onClick(this.props.id)}>X</button>
-      </div>
-    );
-  }
-}
 
 class App extends Component {
   constructor(props) {
@@ -56,7 +15,7 @@ class App extends Component {
 
   handleClicks(id) {
     //TODO add id back to 'pool?'
-    const portfolioCount = this.state.portfolioCount;
+    var newPortfolioCount = this.state.portfolioCount;
     var newPortfolios = this.state.portfolios;
     const nextId = this.state.nextId;
     console.log("Handling Clicks id:" + id + " newPortfolios length:" + newPortfolios.length);
@@ -66,6 +25,7 @@ class App extends Component {
       if (newPortfolios[i].props.id === id) {
         newPortfolios.splice(i, 1);
         removed = true;
+        newPortfolioCount--;
         break;
       }
     }
@@ -76,7 +36,7 @@ class App extends Component {
 
     this.setState(
       {
-        portfolioCount: portfolioCount,
+        portfolioCount: newPortfolioCount,
         portfolios: newPortfolios,
         nextId: nextId
       }
@@ -87,41 +47,31 @@ class App extends Component {
     const portfolioCount = this.state.portfolioCount;
     var newPortfolios = this.state.portfolios;
     const nextId = this.state.nextId;
-    newPortfolios.push(<Portfolio
-      key={nextId}
-      id={nextId}
-      onClick={(id) => this.handleClicks(id)}
-    />);
-    this.setState({
-      portfolioCount: portfolioCount + 1,
-      portfolios: newPortfolios,
-      nextId: nextId + 1,
+    if (portfolioCount < 10) {
+
+      newPortfolios.push(<Portfolio
+        key={nextId}
+        id={nextId}
+        onClick={(id) => this.handleClicks(id)}
+      />);
+      this.setState({
+        portfolioCount: portfolioCount + 1,
+        portfolios: newPortfolios,
+        nextId: nextId + 1,
+      }
+      );
+      console.log("Added Portfolio");
     }
-    );
-    console.log("Added Portfolio");
-    // const newPortfolios = this.state.portfolios;
-    // if (this.state.portfolioCount< 10) {
-    // newPortfolios.push(<Portfolio
-    //   id={nextId}
-    //   onClick={() => this.handleClick(nextId)}
-    // />);
-    // this.setState({ 
-    //   portfolioCount:  portfolioCount + 1,
-    //   portfolios: newPortfolios 
-    // });
-    // this.portfolioCount = portfolioCount + 1;
-    // this.portfolios = newPortfolios;
-    // this.nextId++;
-    // }
   }
 
   render() {
     const portfolios = this.state.portfolios;
     const nextId = this.state.nextId;
+    const portfolioCount = this.state.portfolioCount;
     return (
       <div className="App">
         <header className="App-header">
-          <button id="addPortfolioButton" onClick={() => this.addPortfolio()}>Add Portfolio {nextId}</button>
+          <button id="addPortfolioButton" onClick={() => this.addPortfolio()}>Add Portfolio nr:{portfolioCount} id:{nextId}</button>
           <div className="App-Portfolios">
             {portfolios}
           </div>
